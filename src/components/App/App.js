1c5db4c6d589452da1bc17f69f4967c95c7be398
MZ-87 <zsmile@mail.ru>
2022-11-24 11:02:05 +0300

    added component & styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import './App.css';
+import Price from '../Tariff/Tariff';
+import style from'./app.module.scss';
+import tariffs from '../data/tariffs.json'
+
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
+      {
+        tariffs.map((tariff)=>{
+        return <Price key={tariff.id}  name={tariff.name} price={tariff.price} speed={tariff.speed} info={tariff.info} color={tariff.color} title_color={tariff.title_color}/>
+        })
+      }
     
     </div>
   );
---
+++ src/components/Tariff/Tariff.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+// import style from './tariff.module.scss'
+import './tariff.scss'
+
+export default function Price(props) {
+    let selected = "";
+    const [checked, setChecked] = useState(false);
+    const handleClick = (event) => {
+        setChecked(prevState => !prevState);
+    };
+    selected = checked ? "selected" : "";
+
+    return (
+        <div onClick={handleClick} className={`tariff ${props.color} ${selected}`}>
+            <div className={`tariff__name ${props.title_color}`}>{props.name}</div>
+            <div className="tariff__price">{props.price}</div>
+            <div className="tariff__speed">до {props.speed} Мбит/сек</div>
+            <div className="tariff__info">{props.info}</div>
+        </div>
+    )
+}
+
