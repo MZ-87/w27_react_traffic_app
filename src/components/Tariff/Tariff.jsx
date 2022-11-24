4305c2acb754e5f754ecfe29d983355ec9fb0b04
MZ-87 <zsmile@mail.ru>
2022-11-24 13:48:28 +0300

    import component style module doesn't work

--- src/components/Tariff/Tariff.jsx
+++ src/components/Tariff/Tariff.jsx
@@ -20,3 +20,20 @@ export default function Price(props) {
     )
 }
 
+// export default function Price(props) {
+//     let selected = "";
+//     const [checked, setChecked] = useState(false);
+//     const handleClick = (event) => {
+//         setChecked(prevState => !prevState);
+//     };
+//     selected = checked ? "selected" : "";
+
+//     return (
+//         <div onClick={handleClick} className={`tariff ${props.color} ${selected}`}>
+//             <div className={`tariff__name ${props.title_color}`}>{props.name}</div>
+//             <div className={style.tariff__price}>{props.price}</div>
+//             <div className={style.tariff__speed}>до {props.speed} Мбит/сек</div>
+//             <div className={style.tariff__info}>{props.info}</div>
+//         </div >
+//     )
+// }
